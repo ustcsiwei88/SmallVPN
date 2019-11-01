f671d627e79a21eae09e0874d4d3ec79a74a53e0
Siwei <ustcsiwei88@gmail.com>
2019-11-01 12:49:38 -0400

    server

--- tun.cpp
+++ tun.cpp
@@ -5,9 +5,19 @@
 #include<sys/ioctl.h>
 #include<fcntl.h>
 #include<unistd.h>
-#include<bits/stdc++.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<sys/un.h>
+#include<arpa/inet.h>
+#include<netdb.h>
 
-using namespace std;
+//#include<bits/stdc++.h>
+//using namespace std;
+
+char buff[2048];
 
 int alloc(){
 	struct ifreq ifr;
@@ -22,13 +32,58 @@ int alloc(){
 		close(fd);
 		exit(err);
 	}
-
 	//while(1);
 	return fd;
 }
 
+void server(int fd){
+	// fd: tun fd
+	// sockfd: socket fd
+	// connfd: connection fd
+	int sockfd, connfd;
+	unsigned int len;
+	struct sockaddr_in sa, ta;
+	if((sockfd=socket(AF_INET, SOCK_STREAM, 0))<0){
+		printf("Socket failed");
+		exit(1);
+	}
+	memset(&sa, 0, sizeof(sa));
+	sa.sin_family=AF_INET;
+	sa.sin_port=htons(10010);
+	sa.sin_addr.s_addr=htonl(INADDR_ANY);
+	if(bind(sockfd, (struct sockaddr*)&sa, sizeof(sa))<0){
+		printf("bind error\n");
+		exit(1);
+	}
+	if(listen(sockfd, 10)<0){
+		printf("listen error\n");
+		exit(1);
+	}
+	connfd = accept(sockfd, (struct sockaddr *)&ta, &len);
+	printf("Connection established\n");
+	int pos=0;
+	int sz;
+	while(1){
+		sz = read(connfd, buff, 2048 - pos);
+		if(sz==0){
+			printf("connection ended\n");
+			break;
+		}
+		write(fd, buff+pos, sz);
+		pos+=sz;
+		if(pos==2048) pos=0;
+		printf("read %d %c",sz, buff[0]);
+	}
+}
+void client(){
+	//socket();
+	//connect();
+	//
+
+}
 int main(int argc, char* argv[]){
 	//cout<<argc<<endl;
-	alloc();
+	int fd = alloc();
+	server(fd);
 	return 0;
 }

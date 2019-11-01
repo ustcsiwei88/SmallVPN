24c4fd247c623f87d63fd5cddd09e7f6337a2526
Siwei <ustcsiwei88@gmail.com>
2019-11-01 13:23:23 -0400

    client

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 tun: tun.cpp
-	g++ -std=c++11 tun.cpp -o tun
+	g++ -std=c++11 tun.cpp -o tun -lpthread
--- tun.cpp
+++ tun.cpp
@@ -13,11 +13,16 @@
 #include<sys/un.h>
 #include<arpa/inet.h>
 #include<netdb.h>
+#include<pthread.h>
 
 //#include<bits/stdc++.h>
 //using namespace std;
 
-char buff[2048];
+char rdbuff[2048];
+char wrbuff[2048];
+
+int connfd;//connection fd
+int tunfd;
 
 int alloc(){
 	struct ifreq ifr;
@@ -35,12 +40,22 @@ int alloc(){
 	//while(1);
 	return fd;
 }
-
-void server(int fd){
+void* writer(void* argument){
+	int pos=0;
+	int sz;
+	while(1){
+		sz = read(tunfd, wrbuff, 2048-pos);
+		if(sz==0) break;
+		write(connfd, wrbuff + pos, sz);
+		pos+=sz;
+		if(pos==2048) pos=0;
+	}
+}
+void server(){
 	// fd: tun fd
 	// sockfd: socket fd
 	// connfd: connection fd
-	int sockfd, connfd;
+	int sockfd;// connfd;
 	unsigned int len;
 	struct sockaddr_in sa, ta;
 	if((sockfd=socket(AF_INET, SOCK_STREAM, 0))<0){
@@ -61,29 +76,71 @@ void server(int fd){
 	}
 	connfd = accept(sockfd, (struct sockaddr *)&ta, &len);
 	printf("Connection established\n");
+	pthread_t th;
+	pthread_create(&th, NULL, writer, NULL);
 	int pos=0;
 	int sz;
 	while(1){
-		sz = read(connfd, buff, 2048 - pos);
+		sz = read(connfd, rdbuff, 1 - pos);
 		if(sz==0){
 			printf("connection ended\n");
 			break;
 		}
-		write(fd, buff+pos, sz);
+		write(tunfd, rdbuff+pos, sz);
 		pos+=sz;
-		if(pos==2048) pos=0;
-		printf("read %d %c",sz, buff[0]);
+		if(pos==1) pos=0;
+		printf("read %d %c",sz, rdbuff[0]);
 	}
 }
 void client(){
-	//socket();
-	//connect();
-	//
-
+	// fd: tun fd
+	// sockfd: socket fd
+	// connfd: connection fd
+	int sockfd;// connfd;
+	unsigned int len;
+	struct sockaddr_in sa, ta;
+	if((sockfd=socket(AF_INET, SOCK_STREAM, 0))<0){
+		printf("Socket failed");
+		exit(1);
+	}
+	memset(&sa, 0, sizeof(sa));
+	sa.sin_family=AF_INET;
+	sa.sin_port=htons(10010);
+	//sa.sin_addr.s_addr=htonl(INADDR_ANY);
+	inet_pton(AF_INET, "127.0.0.1", &sa.sin_addr);
+	
+	connfd = connect(sockfd, (struct sockaddr *)&ta, sizeof(sa));
+	printf("Connection established\n");
+	pthread_t th;
+	pthread_create(&th, NULL, writer, NULL);
+	int pos=0;
+	int sz;
+	while(1){
+		sz = read(connfd, rdbuff, 1 - pos);
+		if(sz==0){
+			printf("connection ended\n");
+			break;
+		}
+		write(tunfd, rdbuff+pos, sz);
+		pos+=sz;
+		if(pos==1) pos=0;
+		printf("read %d %c",sz, rdbuff[0]);
+	}
 }
 int main(int argc, char* argv[]){
 	//cout<<argc<<endl;
-	int fd = alloc();
-	server(fd);
+	if(argc<2){
+		printf("argument: 1 (client) 0 (server)\n");
+		exit(1);
+	}
+	tunfd = alloc();
+	printf("%d, %s", argc, argv[1]);
+	if(argv[1][0]=='0'){
+		printf("running server now");
+		server();
+	}else if(argv[1][0]=='1'){
+		printf("running client now");
+		client();
+	}
 	return 0;
 }

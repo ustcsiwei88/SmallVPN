a122a7c09f8fcfcfc6b9966abc101d5b745db972
Siwei Feng <ustcsiwei88@gmail.com>
2019-12-12 19:25:30 -0500

    naive encrypt, TODO: fill in tun_ssl.cpp

--- Makefile
+++ Makefile
@@ -1,2 +1,9 @@
+ALL: tun tun_ssl tun_naive
+
 tun: tun.cpp
 	g++ -std=c++11 -O2 tun.cpp -o tun -lpthread
+tun_ssl: tun_ssl.cpp
+	g++ -std=c++11 -O2 tun_ssl.cpp -o tun_ssl -lpthread -lcrypto -lssl
+tun_naive: tun_naive.cpp
+	g++ -std=c++11 -O2 tun_naive.cpp -o tun_naive -lpthread
+
---
+++ tun_ssl.cpp
@@ -0,0 +1,211 @@
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<sys/ioctl.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<netinet/in.h>
+#include<sys/socket.h>
+#include<sys/un.h>
+#include<arpa/inet.h>
+#include<netdb.h>
+#include<pthread.h>
+#include<linux/if.h>
+#include<linux/if_tun.h>
+
+#include<openssl/bio.h>
+#include<openssl/ssl.h>
+#include<openssl/err.h>
+
+//#include<bits/stdc++.h>
+
+using namespace std;
+ 
+#define MAX_BUFF 2048
+
+char rdbuff[MAX_BUFF];
+char wrbuff[MAX_BUFF];
+
+int connfd;//connection fd
+int tunfd;
+
+BIO *bio;
+
+int alloc(){
+	struct ifreq ifr;
+	int fd, err;
+	if((fd = open("/dev/net/tun", O_RDWR))<0){
+		printf("Open tun failed\n");
+		exit(1);
+	}
+	ifr.ifr_flags=IFF_TUN;
+	strncpy(ifr.ifr_name, "tun0", IFNAMSIZ);
+	if((err=ioctl(fd, TUNSETIFF, (void*)&ifr))<0){
+		close(fd);
+		exit(err);
+	}
+	//while(1);
+	return fd;
+}
+void* writer(void* argument){
+	int pos=0;
+	int sz;
+	while(1){
+		sz = read(tunfd, wrbuff + sizeof(int)/*+pos*/, MAX_BUFF - sizeof(int) /*-pos*/);
+		if(sz==0){
+			printf("writer terminated\n");
+			break;
+		}
+
+	}
+}
+void server(){
+	// fd: tun fd
+	// sockfd: socket fd
+	// connfd: connection fd
+	int sockfd;// connfd;
+	unsigned int len;
+	struct sockaddr_in sa, ta;
+	if((sockfd=socket(PF_INET, SOCK_STREAM, IPPROTO_TCP))<0){
+		printf("Socket failed\n");
+		exit(1);
+	}
+	memset(&sa, 0, sizeof(sa));
+	sa.sin_family=AF_INET;
+	sa.sin_port=htons(10010);
+	sa.sin_addr.s_addr = INADDR_ANY;
+	if(bind(sockfd, (struct sockaddr*)&sa, sizeof(sa))<0){
+		printf("bind error\n");
+		exit(1);
+	}
+	if(listen(sockfd, 10)<0){
+		printf("listen error\n");
+		exit(1);
+	}
+	if((connfd = accept(sockfd, (struct sockaddr *)&ta, &len))<0){
+		printf("Connection error\n");
+		close(sockfd);
+		exit(1);
+	}
+	printf("Connection established, fd: %d\n", connfd);
+	pthread_t th;
+	pthread_create(&th, NULL, writer, NULL);
+	int pos=0;
+	int sz, sz2;
+	while(1){
+		// printf("Wanna receive stuffs\n");
+		if(read(connfd, &sz, sizeof(int))==0){
+			printf("Connection terminated\n");
+			close(connfd);
+			close(sockfd);
+			break;
+		}
+		// printf("sz = %d\n", sz);
+		pos=0;
+		while(pos<sz){
+			pos+=read(connfd, rdbuff + pos, sz-pos /*- pos*/);
+		}
+		// printf("receive sized %d\n", sz2);
+		pos=0;
+		while(pos<sz && pos>=0){
+			pos+=write(tunfd, rdbuff + pos, sz-pos);
+		}
+		// printf("write to tun %d\n",sz2);
+	}
+}
+void client(const char * ipaddr){
+	// fd: tun fd
+	// sockfd: socket fd
+	// connfd: connection fd
+/*	
+	bio = BIO_new_connect(ipaddr_port);
+	if (bio==NULL){
+		printf("bio new connect failed"); exit(1);
+	}
+
+	if(BIO_do_connect(bio)<=0){
+		printf("bio connect failed"); exit(1);
+	}
+	
+	
+	
+*/	
+	
+	
+	
+	
+	
+	
+	int sockfd;// connfd;
+	unsigned int len;
+	struct sockaddr_in sa, ta;
+	if((sockfd=socket(PF_INET, SOCK_STREAM, IPPROTO_TCP))<0){
+		printf("Socket failed");
+		exit(1);
+	}
+	memset(&sa, 0, sizeof(sa));
+	sa.sin_family=AF_INET;
+	sa.sin_port=htons(10010);
+	//sa.sin_addr.s_addr=htonl(INADDR_ANY);
+	inet_pton(AF_INET, ipaddr, &sa.sin_addr);
+	printf("Connecting to %s ...\n", ipaddr);
+	connfd = connect(sockfd, (struct sockaddr *)&sa, sizeof(sa));
+	if(connfd<0){
+		printf("Connection error\n");
+		close(sockfd);
+		exit(1);
+	}
+	connfd = sockfd;
+	printf("Connection established, fd: %d\n", connfd);
+	pthread_t th;
+	pthread_create(&th, NULL, writer, NULL);
+	int pos=0;
+	int sz;
+	while(1){
+		// printf("Wanna receive stuffs\n");
+		if(read(connfd, &sz, sizeof(int))==0){
+			printf("connection terminated\n");
+			close(connfd);
+			close(sockfd);
+			break;
+		}
+		pos=0;
+		// printf("sz = %d\n", sz);
+		while(pos<sz){
+			// printf("reading %d\n", pos);
+			pos+=read(connfd, rdbuff+pos, sz-pos);
+		}
+		pos=0;
+		while(pos<sz && pos>=0){
+			// printf("HERE %d %d\n",sz, pos);
+			pos+=write(tunfd, rdbuff+pos, sz-pos);
+		}
+		// printf("client write finished\n");
+		// printf("received sized %d\n", sz);
+		// pos+=sz;
+		// if(pos==2048) pos=0;
+		//printf("read %d %c",sz, rdbuff[0]);
+	}
+}
+int main(int argc, char* argv[]){
+	//cout<<argc<<endl;
+	if(argc<2 || argc==2 && argv[1][0] == '1'){
+		printf("Argument: 1 (client) server ip addr:port or 0 (server)\n");
+		exit(1);
+	}
+	tunfd = alloc();
+	//printf("%d, %s", argc, argv[1]);
+	SSL_load_error_strings();
+	ERR_load_BIO_strings();
+	OpenSSL_add_all_algorithms();
+	if(argv[1][0]=='0'){
+		printf("running server now\n");
+		server();
+	}else if(argv[1][0]=='1'){
+		printf("running client now\n");
+		client(argv[2]);
+	}
+	return 0;
+}
